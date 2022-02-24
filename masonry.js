08feb5bb7b3c52d0f3064e63489d959d0f071e10
gary royal <gary.l.royal@gmail.com>
2022-02-24 09:53:19 -0600

    added gpl notice

--- masonry.js
+++ masonry.js
@@ -1,4 +1,13 @@
 
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+
 function get_window_geometry() {
 
     viewport_width = function() {
