de72b5de93a294f3883d730890cf7b5cf24eb6c8
Unfrozen Caveman Programmer <glroyal@protonmail.com>
2022-09-10 09:22:58 -0500

    CHTML-Masonry v2 JavaScript

--- masonry.js
+++ masonry.js
@@ -1,12 +1,27 @@
-
 /***************************************************************************
  *
+ *   masonry.js v2
+ *   https://github.com/glroyal/chtml-masonry/shapefile.js
+ *   Copyright (c) 2022 Gary Royal
+ *  
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation; either version 2 of the License, or
  *   (at your option) any later version.
  *
  ***************************************************************************/
+
+
+function $(el) {
+    try {
+        return (typeof el == 'string') ? document.getElementById(el) : el;
+    } catch (e) {
+        if (debug) {
+            alert(el);
+        }
+    }
+} 
+
 
 function get_window_geometry() {
 
@@ -58,8 +73,118 @@ function get_window_geometry() {
     window_width = viewport_width - scrollbar_width;
 };
 
+get_window_geometry();
 
-// prevent rendering until window is resized
+
+const last_width = window_width,
+
+    responsive_columns = [0,0,2,2,2,2,3,3,4,4,5,5,5,5,6,6,7,7,8,8,9],
+
+    columns_per_row = responsive_columns[Math.floor(window_width / 100)],
+
+    gutter_size = 8,
+
+    total_gutter_width = (columns_per_row + 1) * gutter_size,
+
+    max_img_width = (Math.floor((window_width - total_gutter_width) / columns_per_row) * 4) / 4,
+
+    img_width = (max_img_width > 192) ? 192 : max_img_width,
+
+    gallery_width = (img_width * columns_per_row) + total_gutter_width,
+
+    left_offset = Math.floor((window_width - gallery_width) / 2),
+
+    observer = lozad();
+
+
+var page_number = 0, 
+
+    page_length = 32, 
+
+   // page_length = (page_length % columns_per_row > 0) ? Math.ceil(page_length / columns_per_row) : page_length,
+
+    total_pages = Math.ceil(shapes.length / page_length),
+    
+    photo_counter = 1,
+
+    column_height = new Array(columns_per_row);
+
+    column_height.fill(gutter_size);
+    
+
+function fetch_page() {
+
+    if(page_number < total_pages) {    
+
+        var ll = page_number * page_length, rr = ll + page_length;     
+        return shapes.slice(ll, rr);
+
+    } else {
+        return [];             
+    }               
+}
+
+
+function auto_paginate() {
+
+    if(total_pages > 0) {
+
+        var images = fetch_page();
+
+        if(images.length>0) {
+
+            var chtml = [],
+
+            i,  // current image
+
+            j,  // current column
+
+            img_height;
+
+            for(i = 0; i < images.length; i++) {
+
+                // j = column with shortest height
+
+                j = column_height.indexOf(Math.min(...column_height));
+
+                img_height = Math.round((images[i][1] / images[i][0]) * img_width, 0);
+
+                q = (devicePixelRatio > 1 && images[i][0] >= img_width * devicePixelRatio) ? devicePixelRatio : 1;
+
+                chtml[i] = `<div class="lozad brick" style="top:${
+                        column_height[j]
+                    }px;left:${
+                        left_offset + gutter_size + (j * (img_width + gutter_size))
+                    }px;width:${
+                        img_width
+                    }px;height:${
+                        img_height
+                    }px;background-image:url('https://picsum.photos/seed/${
+                        photo_counter
+                    }/${
+                        q * img_width
+                    }/${
+                        q * img_height
+                    }');"><div class="brick-id">${ 
+                        photo_counter + ((q > 1) ? '&nbsp;' + devicePixelRatio + 'x' : '') 
+                    }</div></div>`;
+                        
+                column_height[j] += img_height + gutter_size;
+
+                photo_counter++;
+            }
+
+            var el = document.createElement('div');
+            el.innerHTML = chtml.join('');
+            $('gallery').appendChild(el);
+            
+            observer.observe();        
+        } 
+    } 
+}
+
+
+// pause execution while window is being dragged
 
 function debounce(func){
 
@@ -72,7 +197,7 @@ function debounce(func){
 }
 
 
-// re-render gallery on resize or orientation change
+// re-render gallery after resize or orientation change
 
 window.addEventListener("resize",debounce(function(e){
 
@@ -82,72 +207,34 @@ window.addEventListener("resize",debounce(function(e){
         location.reload();
     }
 }));
-
-
-function render_gallery() {
-
-    const responsive_columns = [0,0,2,2,2,2,3,3,4,4,5,5,5,5,6,6,7,7,8,8,9],
-
-        columns_per_row = responsive_columns[Math.floor(window_width / 100)],
-
-        gutter_size = 8,
-
-        total_gutter_width = (columns_per_row + 1) * gutter_size,
-
-        max_img_width = (Math.floor((window_width - total_gutter_width) / columns_per_row) * 4) / 4,
-
-        img_width = (max_img_width > 192) ? 192 : max_img_width,
-
-        hi_def = (devicePixelRatio > 1),
-
-        gallery_width = (img_width * columns_per_row) + total_gutter_width,
-
-        left_offset = Math.floor((window_width - gallery_width) / 2),
-
-        column_height = new Array(columns_per_row),
-
-        chtml = new Array(images.length);
-
-    var i,  // current image
-
-        j,  // current column
-
-        img_height;
-
-    column_height.fill(gutter_size); // start the wall with a row of mortar
-
-    for(i = 0; i < images.length; i++) {
-
-        j = i % columns_per_row;
-
-        img_height = Math.round((images[i][1] / images[i][0]) * img_width, 0);
-
-        chtml[i] = `<div class="brick" style="top:${
-                column_height[j]
-            }px;left:${
-                left_offset + gutter_size + (j * (img_width + gutter_size))
-            }px;width:${
-                img_width
-            }px;height:${
-                img_height
-            }px;background-image:url('https://picsum.photos/seed/${
-                i+1
-            }/${
-                ((hi_def) ? img_width * 2 : img_width )
-            }/${
-                ((hi_def) ? img_height * 2 : img_height )
-            }');"><div class="brick-id">${ i+1 }</div></div>`;
-                
-        column_height[j] += img_height + gutter_size;
-    }
-    
-    document.getElementById('gallery').innerHTML = chtml.join('');
-}
-
-get_window_geometry();
-
-last_width = window_width;
+      
 
 document.addEventListener("DOMContentLoaded", function(){
-    render_gallery();
+
+    $('form1').innerHTML = [
+
+        '<header>',
+            '<h3>',document.title,'</h3>',
+        '</header>',
+
+        '<div id="pga">',
+            '<div id="gallery"></div>',  
+        '</div>',
+           
+    ].join('');
+
+    $('pga').addEventListener("scroll", function () {
+
+        if ($('pga').scrollHeight - $('pga').scrollTop === $('pga').clientHeight) {
+
+            page_number++; 
+
+            page_number = (page_number>(total_pages-1)) ? total_pages-1 : page_number; 
+
+            auto_paginate();
+        }
+
+    }, false);
+
+    auto_paginate();
 });
